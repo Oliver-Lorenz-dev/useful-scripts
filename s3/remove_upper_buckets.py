0b5b29d9bd2df82ccb846e41618d34420630f8ec
Oliver-Lorenz-dev <85516197+Oliver-Lorenz-dev@users.noreply.github.com>
2021-11-09 13:38:50 +0000

    add --force option

--- s3/remove_upper_buckets.py
+++ s3/remove_upper_buckets.py
@@ -8,7 +8,7 @@ with open ("buckets.txt", 'r') as f:
         for line in data:
             upper_bucket = line.strip()
             # empty bucket
-            empty_bucket_command = "s3cmd del --recursive " + upper_bucket
+            empty_bucket_command = "s3cmd del --recursive --force " + upper_bucket
             os.system(empty_bucket_command)
             # delete bucket
             delete_bucket_command = "s3cmd rb " + upper_bucket
