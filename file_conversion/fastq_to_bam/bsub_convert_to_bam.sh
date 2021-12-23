9930dec46899154a566615480140c3d75b57d7b1
Oliver-Lorenz-dev <oliverlorenz.business@gmail.com>
2021-12-23 14:37:04 +0000

    cleanup

--- file_conversion/fastq_to_bam/bsub_convert_to_bam.sh
+++ file_conversion/fastq_to_bam/bsub_convert_to_bam.sh
@@ -2,7 +2,7 @@
 
 module load bwa/0.7.17-r1188 samtools/1.9
 
-for file in ./SRR*.sh; do
+for file in *_fastq_to_bam.sh; do
 bsub -J fastq_to_bam -o %J.out -e %J.err -R "select[mem>8000] rusage[mem=8000] -M8000 \
 -q normal $file
 done
